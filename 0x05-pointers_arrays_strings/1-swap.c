df90e8b17e8a3b645f87937ce20de42af503c642
martintuoyo <martintuoyo@outlook.com>
2021-06-29 07:47:45 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps int a and int b
+ * 
+ * @a: Becoming a c wiz
+ *
+ * @b: second parameter
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+  int x;
+  
+  x = *a;
+  *a = *b;
+  *b = x;
+  return;
+}
